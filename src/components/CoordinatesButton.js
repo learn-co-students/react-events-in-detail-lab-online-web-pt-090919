31451fd2a6cfad4b3835ac2401164ff3130e21c1
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-05-19 04:23:05 +0000

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,19 @@
 // Code CoordinatesButton Component Here
+import React from 'react';
+
+export default class CoordinatesButton extends React.Component {
+  constructor(props){
+    super(props)
+  }
+  
+  render(){
+    return (
+    <button onMouseMove={this.handleCoordinates}>Receive coordinates</button>  
+    )
+  }
+  
+  handleCoordinates = (event) => {
+    return let coordinates = [event.clientX, event.clientY]
+  }
+  
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,7 @@
 // Code DelayedButton Component Here
+
+import React from 'react';
+
+export default class CoordinatesButton extends React.Component {
+  
+}
