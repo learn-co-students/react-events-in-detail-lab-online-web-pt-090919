17db3c97c93798a4ac27c8ec8effafecfc2cc41b
Randy <randyisbusy@gmail.com>
2020-05-22 12:59:01 -0700

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,11 +1,12 @@
 import React from 'react';
 export default class DelayedButton extends React.Component{  
-    funk = (event) => { 
-            let persistant = event 
+    funk = (event) => {
+            
+            let persistant = event
             console.log("i am persistant", persistant)
         setTimeout((persistant) => { 
             console.log("this", this)
-            console.log(persistant)
+            console.log("persistant in timeout", persistant)
             this.props.onDelayedClick(persistant)
         }, this.props.delay);
     }
